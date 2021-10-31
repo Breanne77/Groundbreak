321c0bad262fa50db21cbdd4db5ed749ac05be8e
Boris <nedev@ualberta.ca>
2021-10-31 16:38:19 -0600

    tried to fix merge conflict

--- js/plugins/YEP_BattleEngineCore.js
+++ js/plugins/YEP_BattleEngineCore.js
@@ -4068,7 +4068,7 @@ Game_Battler.prototype.spriteFaceForward = function() {
 };
 
 Game_Battler.prototype.spriteFaceBackward = function() {
-    this.setMirror(true);
+    this.setMirror(false);
 };
 
 Game_Battler.prototype.spriteFacePoint = function(pointX, pointY) {
